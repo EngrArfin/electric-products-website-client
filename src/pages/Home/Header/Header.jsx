81afcf0b902905199bbc112cd2e5bc8ed02beb05
EngrArfin <arfin.cse.green.edu.bd@gmail.com@>
2024-04-20 14:16:16 +0600

    header update

--- src/pages/Home/Header/Header.jsx
+++ src/pages/Home/Header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
                 <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
                     <h1 className="text-5xl font-bold">Electric Shop Product!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <p className="py-6">Electric Shop Product for buy here. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary">Buy Product</button>
                 </div>
             </div>
         </div>
